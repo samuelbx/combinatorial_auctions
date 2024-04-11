b5c10ec722b2b94bb92013b33ab1401bed39c899
Samuel Boïté <samuel.boite@polytechnique.edu>
2024-04-11 11:54:29 -0400

    simplified example

--- example_3.py
+++ example_3.py
@@ -1,15 +1,16 @@
 from auctions.computations import solve
 from auctions.valuation_classes import gen_zero, gen_additive
 
-epsilon = 1e-6
-alpha, beta, delta, = 1 - epsilon/2, 1 - epsilon/2, 1
+epsilon = 1e-9
+alpha = 1
+x = alpha/epsilon
 
-v1 = [(1, [0, 1, 1, 1, 1+delta, 1+delta, 1+delta, 2+delta])]
+v1 = [(1, [0, 1, 1, 1, 1, 1, 1, 2])]
 v2 = [(1/3, gen_additive((1, 1, 0))),
       (1/3, gen_additive((0, 1, 1))),
       (1/3, gen_additive((1, 0, 1)))]
 v3 = [(1-epsilon, gen_zero(3)),
-      (epsilon, gen_additive((alpha/epsilon, alpha/epsilon, alpha/epsilon)))]
+      (epsilon, gen_additive((x, x, x)))]
 
 solve(valuations = [v1, v2, v3],
       len_items = 3,
