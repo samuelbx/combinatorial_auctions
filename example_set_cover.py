0b3bb2409684da42cbc7bd78b639fc336f1cf373
Samuel Boïté <samuel.boite@polytechnique.edu>
2024-04-17 16:57:20 -0400

    set cover example upd

--- example_set_cover.py
+++ example_set_cover.py
@@ -1,5 +1,11 @@
+from auctions.utils import possible_bundles_names
 from auctions.valuation_classes import gen_subadd_set_cover
 
-print('1 item', gen_subadd_set_cover(2**1-1))
-print('3 items', gen_subadd_set_cover(2**2-1))
-print('7 items', gen_subadd_set_cover(2**3-1))
+def gen(k):
+  m = 2**k - 1
+  names = possible_bundles_names(m)
+  val = gen_subadd_set_cover(m)
+  print(f'{m} item(s)', list(val))
+
+for k in range(1, 4):
+  gen(k)
