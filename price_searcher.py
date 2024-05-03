48f3ccf3ebe1042609f11ca1667c9cb8892f80e4
Samuel Boïté <samuel.boite@polytechnique.edu>
2024-05-02 23:49:07 -0400

    better example

--- price_searcher.py
+++ price_searcher.py
@@ -1,10 +1,9 @@
 from auctions.computations import search_prices
 
-eps = 1e-6
 v0 = [(1, [0, 1, 0, 0, 1, 1, 1, 1])]
 v1 = [(1, [0, 0, 1, 0, 1, 1, 1, 1])]
 v2 = [(1, [0, 0, 0, 1, 1, 1, 1, 1])]
-v3 = [(1, [0, 1+eps, 1+eps, 1+eps, 1+eps, 1+eps, 1+eps, 1+eps])]
+v3 = [(1, [0, 2, 2, 2, 2, 2, 2, 2])]
 
 # Lazy: stops whenever finds prices such that CR >= 2/3
 search_prices(VV = [v0, v1, v2, v3],
