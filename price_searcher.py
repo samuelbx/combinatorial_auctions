aaa9af1fadcca746e532e1e62fa85a7d79bb57fc
Samuel Boïté <samuel.boite@polytechnique.edu>
2024-05-09 15:47:19 -0400

    75% CR example

--- price_searcher.py
+++ price_searcher.py
@@ -1,12 +1,19 @@
 from auctions.computations import search_prices
+from random import random
 
-v0 = [(1, [0, 1, 0, 0, 1, 1, 1, 1])]
-v1 = [(1, [0, 0, 1, 0, 1, 1, 1, 1])]
-v2 = [(1, [0, 0, 0, 1, 1, 1, 1, 1])]
-v3 = [(1, [0, 2, 2, 2, 2, 2, 2, 2])]
+x = .5
+v1 = [(1, [0, 1, 1, 1, 1, 1, 1, 2])]
+v2 = [(1, [0, x, x, x, x, x, x, x])]
+VV = [v1, v2]
 
-# Lazy: stops whenever finds prices such that CR >= 2/3
-search_prices(VV = [v0, v1, v2, v3],
-              mm = 3,
-              method = 'thresholds',
-              lazy = True)
+mu = .1
+for v in VV:
+  inner = v[0][1]
+  for m in range(1, len(inner)):
+    inner[m] += random()*mu
+
+search_prices(VV, mm = 3,
+              method = 'brute',
+              lazy = True,
+              lazy_thres = 1 - 1e-6,
+              grid_points = 50)
